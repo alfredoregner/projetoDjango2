f6ded5d7d34e1788115ef37945fabf056f9bac0e
Alfredo Regner <alfredoregner3@gmail.com>
2025-08-20 21:59:38 -0300

    Configurado acesso de admin + página receita_detail

--- receitas/views.py
+++ receitas/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Receita
+
 
 def home(request):
     return render(request, 'receitas/home.html')
+
+def receita_detail(request, id):
+    receita = get_object_or_404(Receita, pk = id)
+    context={
+        'receita': receita,
+    }
+    return render(request, 'receitas/receita_detail.html', context)
