c766f9966817da3fd817f1c125951f1e84363652
Alfredo Regner <alfredoregner3@gmail.com>
2025-08-21 21:42:44 -0300

    Adicionado a pesquisa para encontrar as receitas do site

--- receitas/views.py
+++ receitas/views.py
@@ -11,3 +11,17 @@ def receita_detail(request, id):
         'receita': receita,
     }
     return render(request, 'receitas/receita_detail.html', context)
+
+def pesquisar_receitas(request):
+    query = request.GET.get('q') # pega o que foi digitado no campo de busca
+    resultados = []
+
+    if query:
+        # filtrar receitas que contenham o termo no nome (sem case-sensitive)
+        resultados = Receita.objects.filter(title__icontains=query)
+
+    context = {
+        'query': query,
+        'resultados': resultados,
+    }
+    return render(request, 'receitas/pesquisa.html', context)
